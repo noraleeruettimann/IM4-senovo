f0eafece40dd3e845710377198cd139011dbd572
noraleeruettimann <150013995+noraleeruettimann@users.noreply.github.com>
2025-06-14 14:45:49 +0200

    Update config.php

--- system/config.php
+++ system/config.php
@@ -1,17 +1,1 @@
-<?php
-// config.php
-$host = 'lg2lh4.myd.infomaniak.com';
-$db   = 'lg2lh4_im4cv';  // Change to your DB name
-$user = 'lg2lh4_im4cv';   // Change to your DB user
-$pass = 'h74!2o.dD&?8gMZ';       // Change to your DB pass if needed
-
-try {
-    $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
-    $pdo = new PDO($dsn, $user, $pass);
-    // Optional: Set error mode
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    echo "Database connection error: " . $e->getMessage();
-    exit;
-}
-?>
+X
